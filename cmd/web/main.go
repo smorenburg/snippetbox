148289a83496ab8d13aba09b309d3688f2e7dbc7
Robin Smorenburg <robin@robino.io>
2024-01-18 20:07:08 +0100

    Add command-line flags and serve static files
    The main.go file has been modified to provide a command-line flag for specifying the network address. Additionally, the server has been configured to serve static files from the "ui/static" directory. The base HTML template has also been updated to include links to a CSS stylesheet, a favicon, some Google-hosted fonts, and a JavaScript file.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,18 +1,26 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Print("starting server on :4000")
+	log.Printf("starting server on %s", *addr)
 
-	err := http.ListenAndServe(":4000", mux)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
